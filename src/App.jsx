0a81d0676f5452c2b2817652202f2dacd4a1bb2a
Tuấn Đỗ Văn <dvtuan.dev@gmai.com>
2023-06-09 01:27:52 +0700

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import EditNote from "./pages/EditNote";
+import CreateNote from "./pages/CreateNote";
 
 function App() {
-    return <>App</>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Notes />} />
+                <Route path="/create-note" element={<CreateNote />} />
+                <Route path="/edit-note/:id" element={<EditNote />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
